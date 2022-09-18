963fe9083d67b7aaa5d666665d5481f2ee6dbdec
PhucSmile <mrphuc48@gmail.com>
2022-09-18 10:02:01 +0700

    Second Commit

--- src/router/Routers.js
+++ src/router/Routers.js
@@ -12,8 +12,8 @@ import ProtectedRouter from '../component/hooks/ProtectedRouter';
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />;
-      <Route path="/home" element={<Home />} />
+      <Route path="/food-delivery" element={<Navigate to="/" />} />;
+      <Route path="/" element={<Home />} />
       <Route path="/foods" element={<AllFoods />} />;
       <Route path="/foods/:id" element={<FoodDetails />} />;
       <Route path="/cart" element={<Cart />} />;
